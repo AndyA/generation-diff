60ab1c6bf82f9aee00d9bf7c376cc601d06376bf
Andy Armstrong <andy@hexten.net>
2022-01-24 19:14:12 +0000

    WIP snapshot.

--- bin/mule.js
+++ bin/mule.js
@@ -3,51 +3,145 @@ const _ = require("lodash");
 const Promise = require("bluebird");
 const { BucketBrigade } = require("../lib/tools/bucket-brigade");
 
-async function writeSequence(bb) {
-  console.log(`Writing`);
-  for (const i of _.range(50)) {
-    await Promise.delay(5);
-    const { name, token } = await bb.nextOutput();
-    console.log(`Writing ${name}`);
-    await fs.promises.writeFile(name, JSON.stringify({ i, name }));
-    await token();
-    console.log(`Written ${name}`);
+class ObjectBrigade {
+  constructor(dir, opt = {}) {
+    const { chunkSize, ...bbOpt } = {
+      chunkSize: 10000,
+      extension: ".jsonl",
+      ...opt
+    };
+    this.opt = { chunkSize };
+    this.bb = new BucketBrigade(dir, bbOpt);
+    this.writeQueue = [];
+    this.readQueue = [];
+    this.eof = false;
   }
-  await bb.close();
+
+  async close() {
+    await this.flushWriteQueue();
+    await this.bb.close();
+  }
+
+  async flushWriteQueue() {
+    const { bb, writeQueue } = this;
+    this.writeQueue = [];
+    if (writeQueue.length) {
+      const { name, token } = await bb.nextOutput();
+      const data = writeQueue.map(JSON.stringify).join("\n");
+      await fs.promises.writeFile(name, data);
+      await token();
+    }
+  }
+
+  async fillReadQueue() {
+    const next = await this.bb.nextInput();
+
+    if (!next) {
+      this.eof = true;
+    } else {
+      const { name, token } = next;
+      const data = await fs.promises.readFile(name, "utf8");
+
+      // Each line gets its own completion token
+      this.readQueue = data
+        .split(/\n/)
+        .map(JSON.parse)
+        .map(object => ({ object, token: token.fork() }));
+
+      // Complete the original base token.
+      await token();
+    }
+  }
+
+  async write(doc) {
+    if (this.writeQueue.length >= this.opt.chunkSize)
+      await this.flushWriteQueue();
+    this.writeQueue.push(doc);
+  }
+
+  async read() {
+    if (this.readQueue.length) return this.readQueue.shift();
+    if (this.eof) return null;
+
+    await this.fillReadQueue();
+    if (this.readQueue.length) return this.readQueue.shift();
+    return null;
+  }
+
+  async consume(consumer) {
+    const next = this.read();
+    if (!next) return false;
+    await Promise.resolve(consumer(next.object));
+    await next.token();
+    return true;
+  }
+}
+
+function logger(type, every, delay) {
+  let count = 0;
+  return async () => {
+    if (!(++count % every)) {
+      console.log(`${type} ${count}`);
+      if (delay) await Promise.delay(delay);
+    }
+  };
+}
+
+function makeRecord(id) {
+  return {
+    id: "availability_p002mq9f" + id,
+    source: "appw",
+    kind: "availability_clip",
+    generation: 1159,
+    ETag: "b8b87d1258264cb0a3baa5779f10e17d",
+    Key: "programme_availability_cd/live/application/json/availability/clip/pid.p002mq9f",
+    LastModified: "2019-08-23T08:55:30.000Z",
+    Size: 3051,
+    StorageClass: "STANDARD",
+    rand: 0.9029780401875992
+  };
+}
+
+async function writeSequence(ob) {
+  const log = logger(`write`, 100000);
+  console.log(`Writing`);
+  for (let id = 0; id < 80000000; id++) {
+    await log();
+    await ob.write(makeRecord(id));
+  }
+  await ob.close();
   console.log(`Finished writing`);
 }
 
-async function readSequence(bb, count) {
+async function readSequence(ob) {
+  // await Promise.delay(500);
+  const log = logger(`read`, 100000);
   console.log(`Reading`);
   while (true) {
-    if (count && --count < 1) break;
-    await Promise.delay(20);
-    const next = await bb.nextInput();
+    const next = await ob.read();
     if (!next) break;
-    const { name, token } = next;
-    console.log(`Reading ${name}`);
-    const data = JSON.parse(await fs.promises.readFile(name, "utf8"));
+    const { object, token } = next;
+    await log();
     await token();
-    console.log(`Read ${name}`, data);
   }
+  console.log(`Finished reading`);
 }
 
-async function keepAlive() {
-  while (true) {
-    console.log(`Sleeping`);
-    await Promise.delay(1000);
-  }
+async function main(verb) {
+  const ob = new ObjectBrigade("tmp/bb1");
+  // await ob.bb.init();
+  const actions = {
+    read: readSequence,
+    write: writeSequence,
+    both: ob => Promise.all([writeSequence(ob), readSequence(ob)])
+  };
+
+  const action = actions[verb];
+  if (!action) throw new Error(`Bad action ${verb}`);
+  await action(ob);
 }
 
-async function main() {
-  // setInterval(() => {}, 1 << 30);
-
-  const bb = new BucketBrigade("tmp/bb1", { extension: ".json" });
-  await Promise.all([writeSequence(bb), readSequence(bb)]);
-  await keepAlive();
-}
-
-main()
+main(process.argv.slice(2))
   .catch(e => {
     console.error(`FATAL:`, e);
     process.exit(1);
