15318bc65bde9f26dcdcb18104311d6a12805d41
Andy Armstrong <andy@hexten.net>
2022-01-31 15:40:49 +0000

    Next slurpstate

--- bin/loader.js
+++ bin/loader.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const split2 = require("split2");
 const { ObjectBrigadeStore } = require("../lib/tools/object-brigade");
 
-const file = "/data/scratch/pi2/slurpstate2.txt";
+const file = "/data/scratch/pi2/slurpstate3.txt";
 const db = "/db/scratch/pips.next";
 
 const tabFields = s => s.split(/\t/);
